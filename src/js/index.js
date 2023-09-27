83504d1cc400036429e72b5e791a144d3488112f
Konstantin Mukhin <kostik.mukhin.1995@gmail.com>
2023-09-27 05:41:54 +0300

    layout based on the layout from figma

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import "../css/normalize.css";
+import "../css/reset.css";
+import "../css/style.css";
+import "../sass/style.sass";
---
+++ webpack.config.js
@@ -0,0 +1,115 @@
+const path = require("path");
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+console.log("isDev", isDev);
+
+const filename = (exp) => (isDev ? `[name].${exp}` : `[name].[hash].${exp}`);
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: "all",
+    },
+  };
+
+  if (isProd) {
+    config.minimizer = [
+      new CssMinimizerWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
+  return config;
+};
+
+const cssLoader = (extra) => {
+  const loader = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+    },
+    "css-loader",
+  ];
+
+  if (extra) loader.push(extra);
+
+  return loader;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    main: "/js/index.js",
+  },
+  output: {
+    filename: filename("js"),
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".js", ".css", ".scss", ".sass"],
+    alias: {},
+  },
+  //   devtool: isDev ?? "source-map",
+  devServer: {
+    port: 2307,
+    hot: isDev,
+  },
+  optimization: optimization(),
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: filename("css"),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets/favicon/favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        //правило для работы с image
+        test: /\.(?:png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: "html-loader",
+      },
+      {
+        //правило для работы с scss
+        test: /\.s[ac]ss$/,
+        use: cssLoader("sass-loader"),
+      },
+      {
+        //правило для работы с css
+        test: /\.css$/,
+        use: cssLoader(),
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
