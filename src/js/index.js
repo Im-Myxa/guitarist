5878cf9ddbc5ffeebea9f928bab38245a2b06d00
Константин Мухин <konstantinmuhin@MacBook-Air-Konstantin.local>
2023-09-30 18:06:05 +0400

    add adaptive layout

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,23 @@ import "../css/normalize.css";
 import "../css/reset.css";
 import "../css/style.css";
 import "../sass/style.sass";
+
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    
+    const menu = document.querySelector('.nav-menu'),
+    menuItem = document.querySelector('.nav-menu__list'),
+    burger = document.querySelector('.burger')
+
+    burger.addEventListener('click', ()=> {
+        burger.classList.toggle('burger_active');
+        menu.classList.toggle('nav-menu_active');
+    })
+
+    menuItem.addEventListener('click', (event)=> {
+        if (event.target.tagName === 'A'){
+            burger.classList.toggle('burger_active');
+            menu.classList.toggle('nav-menu_active');
+        }
+    })
+})
